9ad5ec1d43355e50956b525554e74e85646a4ba4
Crowdbotics <team@crowdbotics.com>
2023-02-27 19:04:23 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "crimson-waterfall-39133",
+  url: "https://crimson-waterfall-39133.botics.co",
+  api: "https://crimson-waterfall-39133.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
